92383f1179122a9f4c5f992f797b390eb7cbb55b
RenanSui <renanddtao@gmail.com>
2024-04-16 15:39:51 -0300

    add scroll smooth animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body className={cn('bg-black', RodinPro.className)} data-menu="0">
         <JotaiProvider>{children}</JotaiProvider>
         <Toaster position="top-center" expand={false} />
