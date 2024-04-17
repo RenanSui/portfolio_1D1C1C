3f416753c37d7b00decc6bd48338c076fb683833
RenanSui <renanddtao@gmail.com>
2024-04-17 11:33:38 -0300

    add animation to header icons

--- src/components/layouts/lobby-header.tsx
+++ src/components/layouts/lobby-header.tsx
@@ -7,6 +7,7 @@ import {
   CardSeparator,
 } from '@/features/menu-sections/components/ui/card'
 import { SiteConfig } from '@/types'
+import { AnimatedShell } from '../shells/animated-shell'
 import { CardMenuShell } from '../shells/card-menu-shell'
 import { Icons } from '../ui/icons'
 
@@ -42,16 +43,21 @@ export const LobbyHeader = ({ siteConfig }: LobbyHeaderProps) => {
       </div>
 
       <ul className="relative z-[60] ml-1 mt-8 flex items-center" aria-label="Social Media">
-        {siteConfig.navbarConfig.socials.map((social) => {
+        {siteConfig.navbarConfig.socials.map((social, index) => {
           const Icon = social.icon ? Icons[social.icon] : Icons.externalLink
 
           return (
-            <li className="group mr-2 shrink-0 text-xs" key={`social-${social.title}`}>
-              <a className="block rounded p-1 transition-all duration-100" href={social.href} target="_blank">
-                <span className="sr-only">{social.title}</span>
-                <Icon className="pointer-events-none h-7 w-7 fill-nier-light-800 text-nier-light-800 transition-colors duration-150 group-hover:fill-nier-light-900 group-hover:text-nier-light-900" />
-              </a>
-            </li>
+            <AnimatedShell
+              key={`icon-${index}`}
+              animate={{ opacity: 1, transition: { delay: 0.5 + index * 0.5, duration: 1 } }}
+            >
+              <li className="group mr-2 shrink-0 text-xs" key={`social-${social.title}`}>
+                <a className="block rounded p-1 transition-all duration-100" href={social.href} target="_blank">
+                  <span className="sr-only">{social.title}</span>
+                  <Icon className="pointer-events-none h-7 w-7 fill-nier-light-800 text-nier-light-800 transition-colors duration-150 group-hover:fill-nier-light-900 group-hover:text-nier-light-900" />
+                </a>
+              </li>
+            </AnimatedShell>
           )
         })}
       </ul>
