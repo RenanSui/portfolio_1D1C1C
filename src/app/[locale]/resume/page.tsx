b1e5769edf67620d78ad1b70b870af61fedc280a
RenanSui <renanddtao@gmail.com>
2024-06-08 13:58:00 -0300

    create locale resume page

---
+++ src/app/[locale]/resume/page.tsx
@@ -0,0 +1,19 @@
+import { unstable_setRequestLocale as setRequestLocale } from 'next-intl/server'
+
+export default function ResumePage({
+  params: { locale },
+}: {
+  params: { locale: string }
+}) {
+  setRequestLocale(locale)
+
+  return (
+    <main className="relative flex min-h-screen flex-col text-white">
+      <object
+        data={`/pdf/renansui_cv_${locale}.pdf`}
+        type="application/pdf"
+        className="relative z-[10001] h-full w-full flex-1"
+      />
+    </main>
+  )
+}
