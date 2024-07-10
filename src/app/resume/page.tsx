930da2d6b214ceae3241f4b69781fd69b5fce764
RenanSui <renanddtao@gmail.com>
2024-07-10 15:55:52 -0300

    modify resume page pdf

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ export default function ResumePage() {
   return (
     <main className="relative flex min-h-screen flex-col">
       <object
-        data="pdf/resume_renan_neves_atualizado.pdf"
+        data="pdf/renansui_cv_pt-br.pdf"
         type="application/pdf"
         className="z-[10000] h-full w-full flex-1"
       />
